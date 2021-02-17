57bdbc0d68436674636d16461cd7ecb9afda10ae
Ron Welch <mmcelt@yahoo.com>
2021-02-17 08:55:47 -0700

    17 - Enemy Health

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+	#region Fields & Properties
+
+	[SerializeField] GameObject _healthBarPrefab;
+	[SerializeField] Transform _barPosition;
+	[SerializeField] float _initialHealth, _maxHealth;
+
+	public float CurrentHealth { get; set; }
+
+	Image _healthbar;
+
+	#endregion
+
+	#region Getters
+
+
+	#endregion
+
+	#region Unity Methods
+
+	void Start() 
+	{
+		CreateHealthBar();
+		CurrentHealth = _initialHealth;
+		UpdateHealthbar();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+			DealDamage(5f);
+
+		UpdateHealthbar();
+	}
+	#endregion
+
+	#region Public Methods
+
+	public void DealDamage(float damageReceived)
+	{
+		CurrentHealth = Mathf.Max(CurrentHealth - damageReceived, 0);
+		if (CurrentHealth == 0)
+		{
+			Die();
+		}
+	}
+	#endregion
+
+	#region Private Methods
+
+	void CreateHealthBar()
+	{
+		GameObject newBar = Instantiate(_healthBarPrefab, _barPosition.position, Quaternion.identity);
+		newBar.transform.SetParent(transform);
+
+		EnemyHealthContainer container = newBar.GetComponent<EnemyHealthContainer>();
+		_healthbar = container.FillAmountImage;
+	}
+
+	void UpdateHealthbar()
+	{
+		_healthbar.fillAmount = Mathf.Lerp(_healthbar.fillAmount, CurrentHealth / _maxHealth, Time.deltaTime * 10f);
+	}
+
+	void Die()
+	{
+		CurrentHealth = _initialHealth;
+		_healthbar.fillAmount = 1f;
+		ObjectPooler.ReturnToPool(gameObject);
+	}
+	#endregion
+}
---
+++ Assets/Scripts/Enemy/EnemyHealthContainer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthContainer : MonoBehaviour
+{
+	#region Fields & Properties
+
+	[SerializeField] Image _fillAmountImage;
+
+	public Image FillAmountImage => _fillAmountImage;
+
+	#endregion
+
+	#region Getters
+
+
+	#endregion
+
+	#region Unity Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
